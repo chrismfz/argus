c8253798e89c2ffd0c208bcf2bc23b6587579f33
Christos Panagiotakis <chris@nixpal.com>
2025-07-12 19:28:03 +0300

    clickhouse ch fix

--- flowrecord.go
+++ flowrecord.go
@@ -3,24 +3,24 @@ package main
 import "time"
 
 type FlowRecord struct {
-	TimestampStart   time.Time
-	Proto            uint8
-	TCPFlags         uint8
-	TOS              uint8
-	SrcHost          string
-	SrcPort          uint16
-	SrcHostCountry   string
-	DstHost          string
-	DstPort          uint16
-	DstHostCountry   string
-	PeerSrcAS        uint32
-	PeerDstAS        uint32
-	ASPath           []string
-	Packets          uint64
-	Bytes            uint64
-	PeerDstASName    string
-	PeerSrcASName    string
-	DstAS            uint32
-	SrcHostPTR       string
-	DstHostPTR       string
+        TimestampStart   time.Time  `ch:"timestamp_start"`
+        Proto            uint8      `ch:"proto"`
+        TCPFlags         uint8      `ch:"tcpflags"`
+        TOS              uint8      `ch:"tos"`
+        SrcHost          string     `ch:"src_host"`
+        SrcPort          uint16     `ch:"src_port"`
+        SrcHostCountry   string     `ch:"src_host_country"`
+        DstHost          string     `ch:"dst_host"`
+        DstPort          uint16     `ch:"dst_port"`
+        DstHostCountry   string     `ch:"dst_host_country"`
+        PeerSrcAS        uint32     `ch:"peer_src_as"`
+        PeerDstAS        uint32     `ch:"peer_dst_as"`
+        ASPath           []string   `ch:"as_path"`
+        Packets          uint64     `ch:"packets"`
+        Bytes            uint64     `ch:"bytes"`
+        PeerDstASName    string     `ch:"peer_dst_as_name"`
+        PeerSrcASName    string     `ch:"peer_src_as_name"`
+        DstAS            uint32     `ch:"dst_as"`
+        SrcHostPTR       string     `ch:"src_host_ptr"`
+        DstHostPTR       string     `ch:"dst_host_ptr"`
 }
